39fa978cee370485846be61ab9bd7f9d6a84097f
leah-jala <leahlafond@protonmail.com>
2022-09-07 18:08:53 +0000

    Add variables for worksheets, move instructions out of main

--- run.py
+++ run.py
@@ -11,17 +11,24 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('gradebook')
 
-raw_grades = SHEET.worksheet('target_averages')
+wks_raw_data = SHEET.worksheet("grades_raw")
+wks_class_ave = SHEET.worksheet("target_averages")
+wks_adjusted = SHEET.worksheet("grades_adjusted_and_final")
+wks_advising = SHEET.worksheet("final_result_needed")
+
 def main():
-    instructions = ("""You will be prompted to enter grades for each student for
-    the next homework or exam due.\n
-    After each grade, please confirm if the grade you entered is correct.
-    If you enter an incorrect grade, you will have the chance to enter it again.
-    After all the grades are entered, you will be given the class average, and 
-    and you will be asked if you would like to add a curve to the results.\n
-    The orinal scores and adjusted grades will be added to the spreadsheet on 
-    separate sheets. Students scores will be averaged, with and calculations will be
-    made for what the student needs to gain a pass, reach the class average
-    or acheive an A.""")
-    print(instructions)
+    print("The program is starting")
+
+
+instructions = ("""You will be prompted to enter grades for each student for
+the next homework or exam due.\n
+After each grade, please confirm if the grade you entered is correct.
+If you enter an incorrect grade, you will have the chance to enter it again.
+After all the grades are entered, you will be given the class average, and 
+and you will be asked if you would like to add a curve to the results.\n
+The orinal scores and adjusted grades will be added to the spreadsheet on 
+separate sheets. Students scores will be averaged, with and calculations will be
+made for what the student needs to gain a pass, reach the class average
+or acheive an A.""")
+print(instructions)
 main()
